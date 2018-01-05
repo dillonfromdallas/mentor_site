ef9d4953fbdbdf05d23cd154c7a2382ec9087d10
Dillon Reyna <thedillonreyna@gmail.com>
2018-01-05 01:04:43 -0600

    Removed django-friendship

--- mentor/mentor/settings.py
+++ mentor/mentor/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['0.0.0.0',
 # Application definition
 
 INSTALLED_APPS = [
-'profiles',
+    'profiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
