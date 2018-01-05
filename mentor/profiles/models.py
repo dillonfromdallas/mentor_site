fb3c3b4f0fc2fdf27b03a2a506b1a4808fe0efb2
Dillon Reyna <thedillonreyna@gmail.com>
2018-01-05 01:03:52 -0600

    Fixed UserProfileView. Added Follow model.

--- mentor/profiles/models.py
+++ mentor/profiles/models.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 
 # Create your models here.
+
+
+class UserModelExtension(User):
+
+    def is_following(self, other_user):
+        try:
+            other_username = User.objects.get(username=other_user)
+            test = Follow.objects.get(follower=self.username, followee=other_username)
+            return True
+        except ObjectDoesNotExist:
+            return False
+
+    class Meta:
+        proxy = True
 
 
 class UserProfile(models.Model):
@@ -12,4 +27,6 @@ class UserProfile(models.Model):
     bio = models.CharField(max_length=255, default="")
 
 
-# First get the UserProfile model going, then add in django-friendship
+class Follow(models.Model):
+    follower = models.ForeignKey(User, related_name='following')
+    followee = models.ForeignKey(User, related_name='followers')
--- mentor/profiles/views.py
+++ mentor/profiles/views.py
@@ -1,9 +1,12 @@
 from . import models
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, TemplateView
+
 
 # Create your views here.
 
@@ -13,10 +16,21 @@ class HomeIndexView(TemplateView):
 
 
 class UserProfileView(DetailView):
-    slug_field = "username"
+    slug_field = "username"  # Enables users to search by username
     slug_url_kwarg = slug_field
-    model = models.UserProfile
+    model = User
     template_name = "profiles/user_profile.html"
+
+    def get_object(self, queryset=None):
+        '''Takes the username input, finds a matching User object, and creates/delivers a UserProfile in it's place.'''
+        user = super().get_object()  # Ensures this only works with existing User objects.
+        try:
+            profile = models.UserProfile.objects.get(user=user)
+            return profile
+        except ObjectDoesNotExist:
+            profile = models.UserProfile.objects.create(user=user)
+            profile.save()
+            return profile
 
 
 class UserSignUpView(CreateView):
