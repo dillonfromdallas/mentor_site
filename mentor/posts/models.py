84988af1023475f809e0e080ad40ade15c78f8ca
Dillon Reyna <thedillonreyna@gmail.com>
2018-01-15 00:14:54 -0600

    Allow Post creation, display on user profile.

--- mentor/posts/models.py
+++ mentor/posts/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.functions import datetime
 
 # Create your models here.
+
+
+class UserProfilePost(models.Model):
+    poster = models.ForeignKey(User, related_name="posting_user")
+    message = models.CharField(max_length=255, default="")
+    post_time = datetime.datetime.now
+
+
+class UserPrivateMessage(models.Model):
+    sender = models.ForeignKey(User, related_name="sending_user")
+    receiver = models.ForeignKey(User, related_name="receiving_user")
+    message = models.TextField()
--- mentor/posts/views.py
+++ mentor/posts/views.py
@@ -1,3 +1,30 @@
+from . import models
+from braces.views import LoginRequiredMixin
+from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
 
 # Create your views here.
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    fields = ['message']
+    model = models.UserProfilePost
+    slug_field = "user__username"
+    slug_url_kwarg = 'username'
+    template_name = "posts/create_post.html"
+
+    def get_success_url(self):
+        messages.success(self.request, 'Post created.')
+        return reverse_lazy('userprofile', kwargs={'username': self.request.user})
+
+
+
+class PostFocusView(DetailView):
+    model = models.UserProfilePost
+
+
+def test_view(request, **kwargs):
+    return HttpResponse("View accessed successfully.")
